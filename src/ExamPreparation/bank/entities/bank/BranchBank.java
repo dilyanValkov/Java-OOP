2027c9c6c49d5557cb23801a826dd92d0f665f14
dilyanValkov <dilqnvalkov@gmail.com>
2023-12-30 16:18:52 +0200

    Delete src/ExamPreparation/bank directory

--- src/ExamPreparation/bank/entities/bank/BranchBank.java
+++
@@ -1,8 +0,0 @@
-package bank.entities.bank;
-
-public class BranchBank extends BaseBank {
-    private final static int CAPACITY = 25;
-    public BranchBank(String name) {
-        super(name, CAPACITY);
-    }
-}
--- src/ExamPreparation/bank/entities/bank/CentralBank.java
+++
@@ -1,8 +0,0 @@
-package bank.entities.bank;
-
-public class CentralBank extends BaseBank {
-    private final static int CAPACITY = 50;
-    public CentralBank(String name) {
-        super(name, CAPACITY);
-    }
-}
