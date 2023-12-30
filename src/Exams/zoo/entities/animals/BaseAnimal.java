29ba72b79c76df64eb5b503e2b7356a298c642a6
dilyanValkov <dilqnvalkov@gmail.com>
2023-12-30 16:31:53 +0200

    Java OOP Retake Exam – 18 April 2022

---
+++ src/Exams/zoo/entities/animals/BaseAnimal.java
@@ -0,0 +1,62 @@
+package zoo.entities.animals;
+
+import static zoo.common.ExceptionMessages.*;
+
+public abstract class BaseAnimal implements Animal{
+    private String name;
+    private String kind;
+    private double kg;
+
+    private double price;
+
+    public BaseAnimal(String name, String kind, double kg, double price) {
+        setName(name);
+        setKind(kind);
+        setKg(kg); ;
+        setPrice(price);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+       if (name == null || name.isBlank()){
+           throw new NullPointerException(ANIMAL_NAME_NULL_OR_EMPTY);
+       }
+       this.name = name;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        if (kind == null || kind.isBlank()){
+            throw new NullPointerException(ANIMAL_KIND_NULL_OR_EMPTY);
+        }
+        this.kind = kind;
+    }
+
+    public double getKg() {
+        return kg;
+    }
+
+    public void setKg(double kg) {
+        this.kg = kg;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price <= 0){
+            throw new IllegalArgumentException(ANIMAL_PRICE_BELOW_OR_EQUAL_ZERO);
+        }
+        this.price = price;
+    }
+   public void eat (){
+        kg +=5;
+   }
+}
---
+++ src/Exams/zoo/entities/foods/Meat.java
@@ -0,0 +1,9 @@
+package zoo.entities.foods;
+
+public class Meat extends BaseFood{
+    private final static int CALORIES = 70;
+    private final static double PRICE = 10;
+    public Meat() {
+        super(CALORIES, PRICE);
+    }
+}
